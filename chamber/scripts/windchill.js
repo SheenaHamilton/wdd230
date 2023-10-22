da78dad7a8e11d8c4fed1336e6542e4e5568218c
sheenahamilton <sheenhamilton@gmail.com>
2023-10-21 23:07:34 -0600

    Week 06

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,30 @@
+const tempValue = document.querySelector('#temp-input');
+const windValue = document.querySelector('#wind-input');
+
+tempValue.addEventListener('input', CalculateWindChill);
+windValue.addEventListener('input',CalculateWindChill);
+
+function CalculateWindChill() {
+
+    const windChillinput = document.querySelector('#windchill-value');
+    const windChillIcon = document.querySelector('#windchill i');
+
+	const tempValue = document.querySelector('#temp-input');
+	const windValue = document.querySelector('#wind-input');
+	
+    if ((tempValue.value <= 50) && (windValue.value > 3))
+    {
+        let windchill =  35.74 + (0.6215 * tempValue.value) - (35.75 * (windValue.value ** 0.16)) + (0.4275 * ((tempValue.value) * (windValue.value ** 0.16)));
+
+        let windchillRounded = windchill.toFixed(1);
+
+	    windChillinput.innerHTML =  windchillRounded + "&degF";
+        windChillIcon.classList.remove("show");
+    }
+    else
+    {
+        windChillinput.innerHTML = "N/A";
+        windChillIcon.classList.add("show");
+    }
+
+};
