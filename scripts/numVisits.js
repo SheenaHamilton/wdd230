ca09cd5b0655e368820e36c581c0f59838a2a618
sheenahamilton <sheenhamilton@gmail.com>
2023-10-26 22:54:14 -0600

    visit counter fix

--- scripts/numVisits.js
+++ scripts/numVisits.js
@@ -4,13 +4,12 @@ const visitsNum = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
-if (numVisits !== 0) {
-	visitsNum.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit. 🌟 Welcome 🌟`;
-	visitsNum.textContent = 1;
-}
-
 numVisits++;
+visitsNum.textContent = numVisits;
+
+if (numVisits == 1) {
+	visitsDisplay.textContent = `This is your first visit. 🌟 Welcome 🌟`;
+	visitsNum.textContent = numVisits;
+}
 
 localStorage.setItem("numVisits-ls", numVisits);
