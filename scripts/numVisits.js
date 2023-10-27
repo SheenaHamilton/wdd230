39532291a8f237a04cc82c6dfc7085ba82bb5877
sheenahamilton <sheenhamilton@gmail.com>
2023-10-26 22:49:54 -0600

    number of visits fonts

--- scripts/numVisits.js
+++ scripts/numVisits.js
@@ -1,12 +1,14 @@
 
-const visitsDisplay = document.querySelector(".visits");
+const visitsDisplay = document.querySelector(".firstVisit");
+const visitsNum = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+	visitsNum.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `This is your first visit. 🌟 Welcome 🌟`;
+	visitsNum.textContent = 1;
 }
 
 numVisits++;
