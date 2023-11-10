d6dc0465c5e511e0511284df6d5995bb2447f4c8
sheenahamilton <sheenhamilton@gmail.com>
2023-11-10 02:31:59 -0700

    Week8 form join

---
+++ chamber/scripts/joindate.js
@@ -0,0 +1,14 @@
+
+
+window.addEventListener('load', () => {
+
+    let joinloaddate = document.querySelector("#timestamp");
+
+    //New Date
+    const currentDate = new Date() ;
+    joinloaddate.value = currentDate;
+
+});
+
+
+
